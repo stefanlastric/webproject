1334ca2688ac299adeeadbfb1ec438d51d099944
Stefan <stefanlastric@gmail.com>
2019-11-04 21:03:47 +0100

    spajanje baze

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const express = require ('express')
-const app = express()
 
-app.get('/',(req,res) => res.send('Test 1!'))
+const express = require('express');
+const mongojs = require('mongojs');
+const bodyParser = require('body-parser');
 
-app.listen(process.env.PORT || 3000, () => console.log('App je na portu 3000'))
+var jwt = require('jsonwebtoken');
+
+const app = express();
+const port = process.env.PORT || 3000
+
+const db = mongojs('mongodb+srv://stefanlastric:stefan123@cluster0-1jeht.mongodb.net/test?retryWrites=true&w=majority', []); // heroku param config file
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+var admin_router = express.Router()
+require('./admin.js')(admin_router, db);
+
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+});
+
+app.use('/admin', admin_router);
