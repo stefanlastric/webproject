8bc86bbcde21f415643b66f6918caa0a05c1a5e5
Stefan <stefanlastric@gmail.com>
2019-11-04 20:12:05 +0100

    exppress dodan

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const express = require ('express'          )
+const app = express()
+
+app.get('/',(req,res) => res.send('Test 1!'))
+
+app.listen(3000, () => console.log('App je na portu 3000'))
