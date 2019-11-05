f9b9bd6d1ac319e654674b344fe4a9d58e920ecf
Stefan <stefanlastric@gmail.com>
2019-11-05 11:17:56 +0100

    prepravka zbog heroku

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const express = require('express');
 const mongojs = require('mongojs');
 const bodyParser = require('body-parser');
 
-var jwt = require('jsonwebtoken');
-
 const app = express();
+/*
+
 const port = process.env.PORT || 3000
 
-const db = mongojs('mongodb+srv://stefanlastric:stefan123@cluster0-1jeht.mongodb.net/test?retryWrites=true&w=majority', []); // heroku param config file
+const db = mongojs('mongodb+srv://stefanlastric:stefan123@cluster0-1jeht.mongodb.net/test?retryWrites=true&w=majority', []); 
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+*/
 
-var admin_router = express.Router()
-require('./admin.js')(admin_router, db);
+app.get('/',(req,res) => res.send('Test 1!'))
 
-app.use(function (req, res, next) {
-    console.log('Time:', Date.now())
-    next()
-});
-
-app.use('/admin', admin_router);
+app.listen(process.env.PORT || 3000, () => console.log('App je na portu 3000'))
